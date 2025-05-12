9ca7ed40484193d793b24a019a0ce5efddd938b8
SaahilM06 <87284050+SaahilM06@users.noreply.github.com>
2025-05-11 23:39:18 -0500

    progress

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FileUploader from './components /FileUploader';
+function App() {
+  return (
+    <div>
+      <FileUploader />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components /FileUploader.tsx
@@ -0,0 +1,8 @@
+export default function FileUploader() {
+    return(
+    <div>
+        <input type ="file" />
+    </div>
+    );
+
+}
